0773b4207d33cb60afe49be352cb0144de79b94b
Muhammad Fikri Arifandi <158024864+iFannJR@users.noreply.github.com>
2025-05-04 23:20:59 +0700

    update

--- API/main.py
+++ API/main.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-app.config["MONGO_URI"] = "mongodb://localhost:27017/flutter_auth"
+app.config["MONGO_URI"] = "mongodb://localhost:27017/"
 mongo = PyMongo(app)
 users = mongo.db.users
 
