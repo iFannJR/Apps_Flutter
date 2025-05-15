0d8a5fc86f97d322e53790372c4f6d44a575f8ff
Muhammad Fikri Arifandi <158024864+iFannJR@users.noreply.github.com>
2025-05-15 16:19:50 +0700

    Tes auth

---
+++ flutter_application_1/lib/auth_service.dart
@@ -0,0 +1,83 @@
+// lib/services/auth_service.dart
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+
+class AuthService {
+  final String baseUrl = 'http://10.0.2.2:8000/api'; // Untuk Android Emulator
+  // Gunakan IP komputer Anda jika menggunakan device fisik
+  // contoh: 'http://192.168.1.5:8000/api'
+
+  Future<Map<String, dynamic>> register(String name, String email, String password) async {
+    final response = await http.post(
+      Uri.parse('$baseUrl/register'),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({
+        'name': name,
+        'email': email,
+        'password': password,
+      }),
+    );
+
+    return jsonDecode(response.body);
+  }
+
+  Future<Map<String, dynamic>> login(String email, String password) async {
+    final response = await http.post(
+      Uri.parse('$baseUrl/login'),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({
+        'email': email,
+        'password': password,
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      // Simpan token
+      final prefs = await SharedPreferences.getInstance();
+      await prefs.setString('token', data['data']['token']);
+      return data;
+    } else {
+      throw Exception('Failed to login');
+    }
+  }
+
+  Future<void> logout() async {
+    final prefs = await SharedPreferences.getInstance();
+    final token = prefs.getString('token');
+
+    final response = await http.post(
+      Uri.parse('$baseUrl/logout'),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $token',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      await prefs.remove('token');
+    } else {
+      throw Exception('Failed to logout');
+    }
+  }
+
+  Future<Map<String, dynamic>> getProfile() async {
+    final prefs = await SharedPreferences.getInstance();
+    final token = prefs.getString('token');
+
+    final response = await http.get(
+      Uri.parse('$baseUrl/profile'),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $token',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      throw Exception('Failed to get profile');
+    }
+  }
+}
